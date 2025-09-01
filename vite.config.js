18865bc0762df1c2cd3edc886477184a19096d63
ssart <ssart@sartaj.com>
2025-09-01 11:34:35 +0530

    Update base path in vite.config.js for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Rate-it/', // GitHub Pages base path
+  base: '/', // GitHub Pages base path
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
